65bf5d0ccae0f72d0a8b7f6b3def23a5afdcb8a4
andrey.kozhevnikov <444221@gmail.com>
2022-03-31 13:55:54 +0400

    unit tests

--- src/deck.spec.ts
+++ src/deck.spec.ts
@@ -1,7 +1,48 @@
-import { Deck } from './deck';
+import { Deck, DeckType } from './deck';
+import { CardRank } from './card';
+import * as _ from 'lodash';
 
 describe('Deck', () => {
   it('should be defined', () => {
     expect(new Deck()).toBeDefined();
   });
+
+  it('generate short and not-shuffled deck', () => {
+    let deck = Deck.generate(DeckType.SHORT, false);
+    expect(deck.shuffled).toBe(false);
+    expect(deck.type).toBe(DeckType.SHORT);
+    expect(deck.cards.length).toBe(36);
+    expect(deck.cards[0].rank).toBe(CardRank._6);
+    expect(deck.cards[35].rank).toBe(CardRank.ACE);
+  });
+
+  it('generate short and shuffled deck', () => {
+    let deck = Deck.generate(DeckType.SHORT, true);
+    expect(deck.shuffled).toBe(true);
+    expect(deck.type).toBe(DeckType.SHORT);
+    expect(deck.cards.length).toBe(36);
+
+    // make sure that four aces are somewhere there
+    expect(_.filter(deck.cards, c => c.rank === CardRank.ACE).length).toBe(4);
+  });
+
+  it('generate full and not-shuffled deck', () => {
+    let deck = Deck.generate(DeckType.FULL, false);
+    expect(deck.shuffled).toBe(false);
+    expect(deck.type).toBe(DeckType.FULL);
+    expect(deck.cards.length).toBe(52);
+    expect(deck.cards[0].rank).toBe(CardRank._2);
+    expect(deck.cards[51].rank).toBe(CardRank.ACE);
+  });
+
+  it('generate full and shuffled deck', () => {
+    let deck = Deck.generate(DeckType.FULL, true);
+    expect(deck.shuffled).toBe(true);
+    expect(deck.type).toBe(DeckType.FULL);
+    expect(deck.cards.length).toBe(52);
+
+    // make sure that four aces are somewhere there
+    expect(_.filter(deck.cards, c => c.rank === CardRank.ACE).length).toBe(4);
+  });
+
 });
